0ee96193173b51cf44cef7dfa228033d701a6b99
SanmerDev <sanmer@sanmer.dev>
2024-07-01 15:14:54 +0800

    Remove unused code

--- app/src/main/kotlin/dev/sanmer/app/compat/BuildCompat.kt
+++ app/src/main/kotlin/dev/sanmer/app/compat/BuildCompat.kt
@@ -9,7 +9,4 @@ object BuildCompat {
 
     @get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
     val atLeastS get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-
-    @get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.R)
-    val atLeastR get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
 }
