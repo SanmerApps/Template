01fb69cc64869f6b8e9dddffd8f9f7b4bfe16ef5
SanmerDev <sanmer@sanmer.dev>
2024-07-14 23:16:14 +0800

    Move Tree to `App`

--- app/src/main/kotlin/dev/sanmer/app/App.kt
+++ app/src/main/kotlin/dev/sanmer/app/App.kt
@@ -2,8 +2,6 @@ package dev.sanmer.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import dev.sanmer.app.utils.timber.DebugTree
-import dev.sanmer.app.utils.timber.ReleaseTree
 import timber.log.Timber
 
 @HiltAndroidApp
@@ -16,8 +14,19 @@ class App : Application() {
         }
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        Timber.d("onCreate")
+    class DebugTree : Timber.DebugTree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            super.log(priority, "<APP_DEBUG>$tag", message, t)
+        }
+
+        override fun createStackElementTag(element: StackTraceElement): String {
+            return super.createStackElementTag(element) + "(L${element.lineNumber})"
+        }
+    }
+
+    class ReleaseTree : Timber.DebugTree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            super.log(priority, "<APP_REL>$tag", message, t)
+        }
     }
 }
