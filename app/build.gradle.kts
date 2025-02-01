e1dc68b5b639d874a553980e4be4660d6c0e1bd4
SanmerDev <sanmer@sanmer.dev>
2025-02-01 17:26:39 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,11 @@ android {
         versionCode = commitCount
 
         ndk.abiFilters += listOf("arm64-v8a", "x86_64")
-        resourceConfigurations += arrayOf("en")
+    }
+
+    @Suppress("UnstableApiUsage")
+    androidResources {
+        localeFilters += listOf("en")
     }
 
     val releaseSigning = if (project.hasReleaseKeyStore) {
